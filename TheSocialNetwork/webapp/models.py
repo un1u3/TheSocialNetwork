cc2ea35682b1e292776cf7d53eff4c7b2bc806dd
Your GitHub Username <un1u3>
2024-12-11 22:25:19 +0545

    Second Commit

--- TheSocialNetwork/webapp/models.py
+++ TheSocialNetwork/webapp/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.text import slugify
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=50)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(unique=True ,blank=True) # blank = true for empty initilization
+    image = models.ImageField(upload_to='images/',blank=True)
     datetime = models.DateField(auto_now_add=True)
+    location = models.CharField(max_length=50,blank=True)
+    
+    def save(self, *args, **kwargs):
+        # Auto-generate slug if it doesn't exist
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        
     
     def __str__(self):
-        return self.user
+        return self.title
     
     
--- TheSocialNetwork/webapp/views.py
+++ TheSocialNetwork/webapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
-def home(request):
-    return render(request,'webapp/home.html')
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'webapp/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.all()
+        return context
+    
+    
